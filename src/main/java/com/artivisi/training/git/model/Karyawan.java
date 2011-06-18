a1006c26aa0319e717ebfef354010041300e0464
Adi-Vbox <adismkn2@gmail.com>
2011-06-19 03:17:14 +0700

    menambahkan model class Karyawan

---
+++ src/main/java/com/artivisi/training/git/model/Karyawan.java
@@ -0,0 +1,33 @@
+package com.artivisi.training.git.model;
+
+public class Karyawan {
+	private String nip;
+	private String name;
+	private String gender;
+	private String telp;
+	public String getNip() {
+		return nip;
+	}
+	public void setNip(String nip) {
+		this.nip = nip;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getTelp() {
+		return telp;
+	}
+	public void setTelp(String telp) {
+		this.telp = telp;
+	}
+	
+}
