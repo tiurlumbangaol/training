62dd2e868034575c0ddafd355c85cf852ca6db7c
Adi-Vbox <adismkn2@gmail.com>
2011-06-19 03:14:57 +0700

    membuat model class Barang

---
+++ src/main/java/com/artivisi/training/git/model/Barang.java
@@ -0,0 +1,27 @@
+package com.artivisi.training.git.model;
+
+public class Barang {
+	private String id;
+	private String kode;
+	private String nama;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getKode() {
+		return kode;
+	}
+	public void setKode(String kode) {
+		this.kode = kode;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+	
+}
